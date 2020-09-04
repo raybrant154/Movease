3ea380d2bf03edbf38a2e1e1b2e7cde7fcf1583e
raybrant154 <raybrant154@gmail.com>
2020-09-04 09:54:51 -0400

    created identity model and added user class

---
+++ Movease.Data/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movease.Data
+{
+   public class User
+    {
+        [Key]
+        public Guid UserId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        //ICollection<MyMovie>
+        //ICollection<Comment>
+
+    }
+}
