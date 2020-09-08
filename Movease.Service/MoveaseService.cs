2c714c17262e23d2906283c0758a8b36494ef9a8
Wes Winn <weswinns@gmail.com>
2020-09-08 11:46:18 -0400

    Wes' MoviesCollection

---
+++ Movease.Service/MoveaseService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movease.Service
+{
+    public class MoveaseService
+    {
+        private readonly Guid _userId;
+
+        public MoveaseService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateMovieCollection
+    }
+}
