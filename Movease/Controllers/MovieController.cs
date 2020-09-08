a741eb06250571f5961983e7828fb12cf4b4b6e1
sgsettle <s.settle41@gmail.com>
2020-09-08 11:14:49 -0400

    movie class

---
+++ Movease/Controllers/MovieController.cs
@@ -0,0 +1,32 @@
+﻿using Movease.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Movease.Controllers
+{
+    public class MovieController : ApiController
+    {
+        static void Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            Task<HttpResponseMessage> getTask = httpClient.GetAsync("http://www.omdbapi.com/?apikey=223a36fc");
+
+            HttpResponseMessage response = getTask.Result;
+
+            HttpResponseMessage getResponse = httpClient.GetAsync("http://www.omdbapi.com/?apikey=223a36fc").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+                Movie movieResponse = response.Content.ReadAsAsync<Movie>().Result;
+            }
+        }
+    }
+}
