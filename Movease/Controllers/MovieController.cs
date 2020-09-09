ebfc5c03158b8d4945e2ebf4a72395e185570a21
sgsettle <s.settle41@gmail.com>
2020-09-09 15:08:49 -0400

    movie class updates

--- Movease/Controllers/MovieController.cs
+++ Movease/Controllers/MovieController.cs
@@ -15,23 +15,6 @@ using System.Web.Http;
 namespace Movease.Controllers
 {
     public class MovieController : ApiController
-    {
-        static void Main(string[] args)
-        {
-            HttpClient httpClient = new HttpClient();
-
-            Task<HttpResponseMessage> getTask = httpClient.GetAsync("http://www.omdbapi.com/?apikey=223a36fc");
-
-            HttpResponseMessage response = getTask.Result;
-
-            HttpResponseMessage getResponse = httpClient.GetAsync("http://www.omdbapi.com/?apikey=223a36fc").Result;
-
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
-
-                Mouse movieResponse = response.Content.ReadAsAsync<Movie>().Result;
-            }
     { // google routing data asp .net
         private MovieService CreateMovieService()
         {
